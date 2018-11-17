b62972edc2ed49c4f92ec90ed258836cbe3f7f54
daisymae <daisymae.orcadiafarm@gmail.com>
2018-11-17 15:22:17 -0600

    create observables
    Create observables from existing data

--- index.ts
+++ index.ts
@@ -1,42 +1,58 @@
-import { Observable } from 'rxjs';
-import { allBooks } from './data';
+import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { allBooks, allReaders } from './data';
 
-// a stand-alone subscriber function
-function subscribe(subscriber) {
+// // a stand-alone subscriber function
+// function subscribe(subscriber) {
 
-  for (let book of allBooks) {
-    subscriber.next(book);
-  }
-}
+//   for (let book of allBooks) {
+//     subscriber.next(book);
+//   }
+// }
 
-// pass the subscribe function to the Observable ctor
-// let allBooksObservable$ = new Observable(subscribe);
+// // pass the subscribe function to the Observable ctor
+// // let allBooksObservable$ = new Observable(subscribe);
 
-// much more common to use arrow function:
-// alternatively could use wrapper function Observable.create in place of 'new Observable'
-// everything else remains the same
-let allBooksObservable$ = new Observable(subscriber => {
-  // 3 methods that exist on Observable interface: next, error, complete
+// // much more common to use arrow function:
+// // alternatively could use wrapper function Observable.create in place of 'new Observable'
+// // everything else remains the same
+// let allBooksObservable$ = new Observable(subscriber => {
+//   // 3 methods that exist on Observable interface: next, error, complete
 
-  // handle errors in observable
-  if (document.title !== 'RxBookTracker') {
-    subscriber.error('Incorrect page title.');
-  }
+//   // handle errors in observable
+//   if (document.title !== 'RxBookTracker') {
+//     subscriber.error('Incorrect page title.');
+//   }
 
-  // handle data
-  for (let book of allBooks) {
-    subscriber.next(book);
-  }
+//   // handle data
+//   for (let book of allBooks) {
+//     subscriber.next(book);
+//   }
 
-  // notify of completion
-  setTimeout(() => {
-    subscriber.complete();
-  }, 2000);
+//   // notify of completion
+//   setTimeout(() => {
+//     subscriber.complete();
+//   }, 2000);
 
-  // need to return to clean up after
-  return () => console.log('Executing teardown code.');
+//   // need to return to clean up after
+//   return () => console.log('Executing teardown code.');
 
-});
+// });
 
-// MUST subscribe for observable to execute
-allBooksObservable$.subscribe(book => console.log(book.title));
+// // MUST subscribe for observable to execute
+// allBooksObservable$.subscribe(book => console.log(book.title));
+
+
+// 'of' returns an observable from data you already have
+let source1$ = of('hello', 10, true, allReaders[0].name);
+
+// source1$.subscribe(value => console.log(value));
+
+let source2$ = from(allBooks);
+
+// source2$.subscribe(book => console.log(book.title));
+
+// how to combine 2 observables into 1; want combined into a single 
+// will produce a single output that will have all the values of 
+// the first followed by all the values of the second
+concat(source1$, source2$)
+  .subscribe(value => console.log(value));
